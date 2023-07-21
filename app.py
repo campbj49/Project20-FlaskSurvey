4149cdeef68ae5841687f2f6d82042bfdc8cfbbc
John Campbell <campbj49@gmail.com>
2023-07-21 11:43:45 -0400

    added session functionality and flash messaging

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import *
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -7,12 +7,12 @@ app.config['SECRET_KEY'] = "chickenz"
 
 #debug = DebugToolbarExtension(app)
 
-responses = []
-drv = []
+#responses = []
 
 @app.route("/")
 def start():
     """Render landing page"""
+    if(session.get("survey", None) != None):del session["survey"]
     return render_template("start.html",
         title = "Landing Page",
         header = "Select Survey",
@@ -21,23 +21,22 @@ def start():
 @app.route("/questions/<num>")
 def run_survey(num):
     """Run survey"""
-    survey_key_name = request.args.get("survey","ERR: Bad survey key")
+    survey_key_name = session.get("survey",False)
+    if(not survey_key_name):
+        flash("No survey selected. Redirecting")
+        return redirect("/")
     current_survey = surveys[survey_key_name]
-    global responses
-    global drv
         
     
-    if(num != str(len(responses)+1) and not (responses ==[] and num == "0")): 
-        drv +=[str(len(responses))+" is the current len and num is " + num]
+    if(num != str(len(session["responses"])+1) and not (session["responses"] ==[] and num == "0")): 
         flash("Attempted to go to invalid question")
-        return redirect("/questions/"+str(len(responses)+1)+"?survey="+survey_key_name)
+        return redirect("/questions/"+str(len(session["responses"])+1))
     if(num == "0"):
         return render_template("survey.html",
             title = "Survey",
             header = current_survey.title,
             instr = current_survey.instructions,
             next_num = 1,
-            survey = survey_key_name,
             button_text="Begin")
             
     elif(int(num) <= len(current_survey.questions)):
@@ -48,7 +47,6 @@ def run_survey(num):
             next_num = int(num)+1,
             choices = cur_question.choices,
             question = True,
-            survey = survey_key_name,
             button_text="Next Question")
             
     else:
@@ -58,33 +56,21 @@ def run_survey(num):
             instr = "Thank you for finishing the survey",
             next_num = "finish",
             finished = True,
-            survey = survey_key_name,
             button_text="Finish")
             
             
 @app.route("/answers")
 def handle_ans():
-    global responses
-    if(request.args.get("ans",False)): responses+=[request.args["ans"]]
-    return redirect("questions/"+str(len(responses)+1)+"?survey="+request.args.get("survey","ERR: Bad survey key"))
+    if(request.args.get("ans",False)): session["responses"]+=[request.args["ans"]]
+    return redirect("questions/"+str(len(session["responses"])+1))
     
+@app.route("/init")
+def initialize():
+    session["survey"] =  request.args.get("survey","ERR: Bad survey key")
+    session["responses"] = []
+    return redirect("/questions/0")
 @app.route("/resp")
 def resp_check():
-    global drv
-    res = "Errors are currently: " + str(len(drv))+ " "
-    for resp in responses: res += resp + " "
+    res = "Survey is currently: " + session.get("survey","undefined")+ " "
+    for resp in session["responses"]: res += resp + " "
     return render_template("resp.html", res = res)
-        
-        
-        
-"""
-This error catching nonsense is really kicking my teeth in. Sketch as it stands:
-instruction page. ignore error if responses are empty
-load q1 resp is still empty. 
-load q2 resp len = 1
-clicking away should redirect to 2, but even if it does it risks re-entering a responses
-think that putting the data collection into a redirect will make this more managable
-How do I prevent navigating to 1 causing an endless loop of redirecting to 0 while also not 
-having navigating to 0 immediatly causing a redirect to 1?
-
-"""
